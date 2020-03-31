c7b5f96b050631c29105a321822bf477459361cd
johan <johan.leon@unillanos.edu.co>
2020-03-31 10:18:32 -0500

    galeria medio funcional

--- MemoAnime/platforms/android/app/src/main/assets/www/js/jsonCards.js
+++ MemoAnime/platforms/android/app/src/main/assets/www/js/jsonCards.js
@@ -1,363 +1,363 @@
 var jsonCards={"info":[
 
-    {"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    {"Nombre":"Nami",
+   "Edad":"20",
+   "Altura":"1.70",
+   "Sexo":"Femenino",
+   "Anime":"One Piece"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Kamado Nezuko",
+   "Edad":"14",
+   "Altura":"1.53",
+   "Sexo":"Femenino",
+   "Anime":"Kimetsu No Yaiba"
     }
-    ,{"Nombre":"",
-   "Edad":"",
+    ,{"Nombre":"Lakyus Alvein Dale Aindra",
+   "Edad":"19",
    "Altura":"",
-   "Sexo":"",
-   "Anime":""
+   "Sexo":"Femenino",
+   "Anime":"Overlord"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Alice Schuberg",
+   "Edad":"19",
+   "Altura":"1.68",
+   "Sexo":"Femenino",
+   "Anime":"Sword Art Online Alicization"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Chihaya Ayase",
+   "Edad":"18",
+   "Altura":"1.67",
+   "Sexo":"Femenino",
+   "Anime":"Chihayafuru"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"uraraka",
+   "Edad":"16",
+   "Altura":"1.56",
+   "Sexo":"Femenino",
+   "Anime":"Boku No Hero Academia"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Asuna Yuuki",
+   "Edad":"18",
+   "Altura":"1.70",
+   "Sexo":"Femenino",
+   "Anime":"Sword Art Online"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Saya Takagi",
+   "Edad":"16",
+   "Altura":"1.55",
+   "Sexo":"Femenino",
+   "Anime":"Highschool Of the Dead"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Violet Evergarden",
+   "Edad":"17",
+   "Altura":"1.68",
+   "Sexo":"Femenino",
+   "Anime":"Violet Evergarden"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Louise Francoise le Blanc de la Vallier",
+   "Edad":"17",
+   "Altura":"1.55",
+   "Sexo":"Femenino",
+   "Anime":"Zero no Tsukaima"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Shana",
+   "Edad":"15 (inmortal)",
+   "Altura":"1.41",
+   "Sexo":"Femenino",
+   "Anime":"Shakagan no Shana"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Ais Wallenstein",
+   "Edad":"16",
+   "Altura":"1.62",
+   "Sexo":"Femenino",
+   "Anime":"DanMachi"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Mikasa Ackerman",
+   "Edad":"19",
+   "Altura":"1.76",
+   "Sexo":"Femenino",
+   "Anime":"Shingeki No Kyojin"
     }
-    ,{"Nombre":"",
+    ,{"Nombre":"Ishtar",
    "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+   "Altura":"1.59",
+   "Sexo":"Femenino",
+   "Anime":"Fate/Grand Order"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Hinata Hyūga",
+   "Edad":"16",
+   "Altura":"1.60",
+   "Sexo":"Femenino",
+   "Anime":"Naruto"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Moka Akashiya",
+   "Edad":"17",
+   "Altura":"1.68",
+   "Sexo":"Femenino",
+   "Anime":"Rosario + Vampire"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Himiko Toga",
+   "Edad":"17",
+   "Altura":"1.57",
+   "Sexo":"Femenino",
+   "Anime":"Boku No Hero Academia"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Erio Towa",
+   "Edad":"16",
+   "Altura":"1.53",
+   "Sexo":"Femenino",
+   "Anime":"Denpa Onna to Seishun Otoko"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Lala Satalin Deviluke",
+   "Edad":"16",
+   "Altura":"1.65",
+   "Sexo":"Femenino",
+   "Anime":"To love ru"
     }
-    ,{"Nombre":"",
-   "Edad":"",
-   "Altura":"",
-   "Sexo":"",
-   "Anime":""
+    ,{"Nombre":"Haruka Morishima",
+   "Edad":"18",
+   "Altura":"1.68",
+   "Sexo":"Femenino",
+   "Anime":"Amagami SS"
     },
-    {"Nombre":"",
+    {"Nombre":"Lujuria",
+    "Edad":"250",
+    "Altura":"",
+   "Sexo":"Femenino",
+    "Anime":"Full Metal Alchemist"
+     }
+     ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      }
      ,{"Nombre":"",
     "Edad":"",
     "Altura":"",
-    "Sexo":"",
-    "Anime":""
-     }
-     ,{"Nombre":"",
-    "Edad":"",
-    "Altura":"",
-    "Sexo":"",
+   "Sexo":"Femenino",
     "Anime":""
      },
      {"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
       ,{"Nombre":"",
      "Edad":"",
      "Altura":"",
-     "Sexo":"",
+   "Sexo":"Femenino",
      "Anime":""
       }
 ]};
